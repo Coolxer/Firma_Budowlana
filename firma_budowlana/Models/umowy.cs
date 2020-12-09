c41a963da77ff040007532c62d290c310c22556e
coolxer <lukasik4789@gmail.com>
2020-12-09 11:15:57 +0100

    update validation

--- firma_budowlana/Models/umowy.cs
+++ firma_budowlana/Models/umowy.cs
@@ -11,7 +11,8 @@ namespace firma_budowlana.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class umowy
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,12 +20,19 @@ namespace firma_budowlana.Models
         {
             this.faktury = new HashSet<faktury>();
         }
-    
+
+        [Required]
         public int id { get; set; }
+
+        [Required]
         public int nr_zlecenia { get; set; }
+
+        [Required]
         public string typ { get; set; }
+
+        [Required]
         public System.DateTime data_wystawienia { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<faktury> faktury { get; set; }
         public virtual zlecenia zlecenia { get; set; }
